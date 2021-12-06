8396bf9f9d25db60e07d21ceb67526b5498ffa0c
JerryKu <jerryku1220@gmail.com>
2021-12-06 03:02:42 -0800

    major updates with themes

--- src/context/TokenContext.jsx
+++ src/context/TokenContext.jsx
@@ -1,5 +1,3 @@
-import axios from 'axios'
-import { round } from 'common/utils/numbers'
 import { getTokenPriceInUSD } from 'common/utils/tokens'
 import React, { useEffect, useState } from 'react'
 import supportedPancakeTokens from '../common/constants/tokens/supportedPancakeTokens.json'
@@ -8,14 +6,7 @@ const TokenContext = React.createContext()
 
 const TokenContextProvider = ({ children }) => {
     const supportedTokenList = supportedPancakeTokens.tokens
-    const [currentlySelectedToken, setCurrentlySelectedToken] = useState({
-        address: '0x1a1d7c7A92e8d7f0de10Ae532ECD9f63B7EAf67c',
-        chainId: 56,
-        decimals: 9,
-        logoURI: 'https://utopia.cc/assets/image/utopia/utopiaUDarkbg.svg',
-        name: 'Utopia Token',
-        symbol: 'UTOPIA',
-    })
+    const [currentlySelectedToken, setCurrentlySelectedToken] = useState(supportedPancakeTokens.tokens[0])
     const [currentTokenPriceInUSD, setCurrentTokenPriceInUSD] = useState()
 
     useEffect(async () => {
